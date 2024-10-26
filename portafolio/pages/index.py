af42376656fab02f133c2adb268e15530910dea9
xsismadn3ss <2477carlos.a@gmail.com>
2024-10-26 14:31:54 -0600

    modularizar pagin de inicio

--- portafolio/pages/index.py
+++ portafolio/pages/index.py
@@ -1,6 +1,7 @@
 import reflex as rx
 from rxconfig import config
-from reflex_github_badge.github_badge import github_badge
+from .views.instructions import instructions
+from .views.demo import demo
 
 
 @rx.page(
@@ -10,41 +11,9 @@ def index() -> rx.Component:
     return rx.vstack(
         rx.color_mode.button(position="top-right"),
         rx.heading("Github Card", size="6"),
-        github_badge(username="xsismadn3ss", description="Hello world 👋"),
+        demo(),
         rx.heading("¿How to use?"),
-        rx.card(
-            rx.heading(
-                "1- Write this command on your terminal to install this package",
-                size="3",
-            ),
-            rx.markdown(
-                r"""
-                ```bash
-                pip install reflex-github-badge
-                ```
-                """
-            ),
-            rx.heading("2- Import reflex-github-badge", size="3"),
-            rx.markdown(
-                r"""
-                ```python
-                from reflex_github_badge.github_badge import github_badge
-                ```
-                """
-            ),
-            rx.heading(
-                "3- Enter your github username and set a description(optional)",
-                size="3",
-            ),
-            rx.markdown(
-                r"""
-                ```python
-                github_badge(username="hello", description="hello world")
-                ```
-                """
-            ),
-            size="2",
-        ),
+        instructions(),
         width="100%",
         align="center",
         justify="center",
